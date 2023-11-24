fe5b96210585f15ebb3813391d052741d655956a
Matias Albarello <1678497+matiasalbarello@users.noreply.github.com>
2023-11-24 16:20:30 +0100

    Fix typo in module names

--- lib/cryptomate_api/management/blockchain.rb
+++ lib/cryptomate_api/management/blockchain.rb
@@ -5,7 +5,7 @@ module CryptomateApi
     # Get all the blockchains available in the platform. You can use this information to create new
     # credentials for your clients or using every other endpoints that ask for the blockchain where
     # you want to operate.
-    class Blockchain < CryptomateAPI::Base
+    class Blockchain < CryptomateApi::Base
       # Get all blockchains
       # https://cryptomate.me/docs/management#get-all-blockchains
       # Response:
