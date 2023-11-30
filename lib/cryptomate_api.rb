49d8743f0ed279f077c26ad139bbbdb9b828e76e
Matias Albarello <1678497+matiasalbarello@users.noreply.github.com>
2023-11-30 11:38:58 +0100

    require configuration before the rest

--- lib/cryptomate_api.rb
+++ lib/cryptomate_api.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require_relative 'cryptomate_api/base'
 require_relative 'cryptomate_api/configuration'
+require_relative 'cryptomate_api/base'
 
 Dir[File.join(__dir__, 'cryptomate_api/{management,mpc,nft}/**/*.rb')].sort.each { |file| require file }
 
