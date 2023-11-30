6045ddb43fa5d5c87bff2ef9f589b3f664d7093d
Matias Albarello <1678497+matiasalbarello@users.noreply.github.com>
2023-11-30 11:39:37 +0100

    add base_uri to configuration

--- lib/cryptomate_api/base.rb
+++ lib/cryptomate_api/base.rb
@@ -3,12 +3,13 @@ require 'httparty'
 module CryptomateApi
   class Base
     include HTTParty
-    base_uri CryptomateApi.configuration.base_uri
 
     # Initializes the client with the given API key
-    def initialize(api_key = CryptomateApi.configuration.api_key)
-      @api_key = api_key
       self.class.headers "Authorization" => "Bearer #{@api_key}"
+    def initialize
+      CryptomateApi.configuration ||= CryptomateApi::Configuration.new
+      @api_key = CryptomateApi.configuration.api_key
+      self.class.base_uri CryptomateApi.configuration.base_uri
     end
   end
 end
